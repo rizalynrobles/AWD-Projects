9ceaf9a4f268159e070a50cb88d883219a028354
riri <155805335+rizalynrobles@users.noreply.github.com>
2024-01-24 20:19:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const scrollIndicator = document.getElementById('_indicator');
+const button = document.getElementById('_button');
+const notification = document.getElementById('_notification');
 
-const totalScrollHeight = document.body.scrollHeight = window.innerHeight;
+button.addEventListener('click', () => {
+const counter = Number(notification.getAttribute('data-count')) || 0;
 
-function UpdateScrollIndicator() {
-    const newScrollHeight = (window.scrollY / totalScrollHeight) = 100;
-    scrollIndicator.style.width = '${newScrollHeight}%';
-}
+notification.setAttribute('data-count', counter + 1);
+notification.classList.add('count')
+notification.classList.add('notify');
+notification.classList.add('active')
 
-window.addEventListener('scroll', UpdateScrollIndicator);
+notification.addEventListener('animationend', () =>{
+notification.classList.remove('notify');
+button.classList.remove('active');
+})
+})
